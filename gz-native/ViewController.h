2ae5440972d27230bf5e2f96e46aca86ed84ed00
Ryan Faerman <ryan@faerman.net>
2012-06-27 21:25:24 -0400

    Initial Commit

---
+++ gz-native/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  gz-native
+//
+//  Created by Ryan Faerman on 6/27/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
